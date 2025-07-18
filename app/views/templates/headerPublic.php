63717ac8bb54d46ba982d53d7955edca7153f842
Utkapatel-algoma <Bittupatel3312@gmail.com>
2025-07-18 12:25:03 +0000

    Assignment 5 (continued from Assignment 4 content)

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -1,19 +1,19 @@
-    <?php
-    if (isset($_SESSION['auth']) == 1) {
-        header('Location: /home');
-    }
-    ?>
-
-    <!DOCTYPE html>
-    <html lang="en">
-    <head> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="/app/views/templates/style.css">
-
-        <link rel="icon" href="/favicon.png">
-        <title>COSC 4806</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="mobile-web-app-capable" content="yes">
-    </head>
-    <body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your App Name - Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <header class="bg-primary text-white p-3 mb-4">
+        <div class="container d-flex justify-content-between align-items-center">
+            <h1 class="h3 mb-0">Your App Name</h1>
+            <nav>
+                <a href="/login" class="text-white mx-2 text-decoration-none">Login</a>
+                <a href="/create" class="text-white mx-2 text-decoration-none">Register</a>
+            </nav>
+        </div>
+    </header>
+    <main class="container">
